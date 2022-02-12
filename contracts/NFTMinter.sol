74e307229712008393c8f7a5f618bc5682a61d1d
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-13 09:48:47 +1000

    Update Contract as ECR721 with Mint Functionality

--- contracts/NFTMinter.sol
+++ contracts/NFTMinter.sol
@@ -2,10 +2,34 @@
 
 pragma solidity ^0.8.0;
 
+//Add Imports
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract NFTMinter {
-    constructor() {
-        console.log("This is my NFT minter contract. Whoa!");
-    }
+contract NFTMinter is ERC721URIStorage {
+
+  // Counter contract to help keep NFTs unique
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // Pass constructor the name of our NFTs token and its symbol.
+  constructor() ERC721 ("DopeGoats", "DPGT") {
+    console.log("This is my Dope Goat NFT contract. Woah!");
+  }
+
+  // A function our user will hit to get their NFT.
+  function makeDGNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current();
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    _setTokenURI(newItemId, "https://jsonkeeper.com/b/WDAO");
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
 }
