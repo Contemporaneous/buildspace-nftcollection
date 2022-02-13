d9f0fd1e88fe8338aab6e4428b384903e3875bbc
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-13 11:31:10 +1000

    Update the run script to getn 10 NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -5,15 +5,13 @@ const main = async () => {
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
   
-    // Call the Minting function
-    let txn = await nftContract.makeDGNFT()
-    // Wait for it to be mined
-    await txn.wait()
+    // Call the Minting function 10 times
+    for (let i = 0; i < 10; i++) {
+        let txn = await nftContract.makeDGNFT()
+        // Wait for it to be mined
+        await txn.wait()
+    }
   
-    // Mint another NFT for fun
-    txn = await nftContract.makeDGNFT()
-    // Wait for it to be mined
-    await txn.wait()
   
   };
   
