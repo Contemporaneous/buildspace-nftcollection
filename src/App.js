82d0fc0e1b468d7704bf1a5cb3021182cc36f227
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-14 17:58:40 +1000

    Setup event listeners

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -57,10 +58,38 @@ const App = () => {
       //set current account
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+
+      setupEventListener() 
     } catch (error) {
       console.log(error)
     }
   }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, NFTMinter.abi, signer);
+
+        connectedContract.on("NFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
   const askContractToMintNft = async () => {
     
